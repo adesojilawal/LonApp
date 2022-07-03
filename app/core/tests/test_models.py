a65245bb46e1295812a8272cbfa8ea8de7bd4cfb
esojay5000 <esojay5000@yahoo.com>
2022-07-03 20:25:05 -0230

    Issue 2 with model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -8,9 +8,8 @@ from django.contrib.auth import get_user_model
 class ModelTests(TestCase):
     """Test models."""
 
-
     def test_create_user_with_email_successful(self):
-        """Test creating a user with an email is successful"""
+        """Test creating a user with an email is successful."""
         email = 'test@example.com'
         password = 'testpass123'
         user = get_user_model().objects.create_user(
